72b374293c740ebc0590d8e43bd95aeea00caeb7
Muhamad Diaz <muhamaddiaz10@gmail.com>
2025-06-07 22:09:38 +0700

    feat: integrate husky and lint-staged for pre-commit hooks

--- packages/repos-hub-default-e2e/playwright.config.ts
+++ packages/repos-hub-default-e2e/playwright.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
-import { workspaceRoot } from '@nx/devkit';
+import { workspaceRoot } from "@nx/devkit";
+import { nxE2EPreset } from "@nx/playwright/preset";
+import { defineConfig, devices } from "@playwright/test";
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4300';
+const baseURL = process.env["BASE_URL"] || "http://localhost:4300";
 
 /**
  * Read environment variables from file.
@@ -15,19 +15,19 @@ const baseURL = process.env['BASE_URL'] || 'http://localhost:4300';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  ...nxE2EPreset(__filename, { testDir: './src' }),
+  ...nxE2EPreset(__filename, { testDir: "./src" }),
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm exec nx run @repos-hub/repos-hub-default:preview',
-    url: 'http://localhost:4300',
+    command: "pnpm exec nx run @repos-hub/repos-hub-default:preview",
+    url: "http://localhost:4300",
     reuseExistingServer: true,
-    cwd: workspaceRoot
+    cwd: workspaceRoot,
   },
   projects: [
     {
@@ -44,7 +44,7 @@ export default defineConfig({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-    
+
     // Uncomment for mobile browsers support
     /* {
       name: 'Mobile Chrome',
--- packages/repos-hub-enhanced/eslint.config.mjs
+++ packages/repos-hub-enhanced/eslint.config.mjs
@@ -1,17 +1,18 @@
 import nx from "@nx/eslint-plugin";
+
 import baseConfig from "../../eslint.config.mjs";
 
 export default [
-    ...baseConfig,
-    ...nx.configs["flat/react"],
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.js",
-            "**/*.jsx"
-        ],
-        // Override or add rules here
-        rules: {}
-    }
+  ...baseConfig,
+  ...nx.configs["flat/react"],
+  {
+    files: [
+      "**/*.ts",
+      "**/*.tsx",
+      "**/*.js",
+      "**/*.jsx",
+    ],
+    // Override or add rules here
+    rules: {},
+  },
 ];
